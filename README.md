aeafae00b711ac4cab8dbcb008862eaf9ae5d131
root <root@DESKTOP-BDPASFJ.localdomain>
2017-09-24 17:00:25 -0700

    Add more files

--- README.md
+++ README.md
@@ -16,9 +16,17 @@ Parameter : k,m,n
 
 This file calculate the value of $phi^{-1}_k(l)$ where $phi_k(i,j) = (k+1)i+kj$, then the closest point to the grid $G_{m,n}$ is found. An example in the paper $3,6,6$ is shown. 
 
+## reveal_spots_minesweeper.py
+Some spots are already revealed for 2-distance minesweeper. 
+
+## limit_sweep_minesweeper.py
+Limit the number of times you can play 1-distance or 2-distance sweep.
+
 Todo: 1. Put this in actual minesweeper board, see what happens.
 2. Observe other behavior and comes up with some conjecture
 
 ## Note/Observation
 
 1. The number presented in the paper show a configuration of the minimum number of bombs needed in an m X n board so that any non-mine cell in k-minesweeper has positive number. This means that any 'click' will open a mine or only a single cell number, which is very challenging.
+
+2. We can formulate the reveal spots problem mathematically as "For any subset S of vertices of grid graph G(m,n). Let f(m,n,k,S): V(G)->N be the function define on each vertex such that f(m,n,k,S) is the number of vertex in S distance k away from vertex (m,n). Let T (such that T intersects S is empty) be a subset of vertices in G(m,n) that we define f(m,n,k,S) on. What is the minimum value of |T| that would make S unique?
---
+++ reveal_spots_minesweeper.py
@@ -0,0 +1,160 @@
+import random
+
+
+class boardSpot(object):
+    value = 0
+    selected = False
+    mine = False
+
+    def __init__(self):
+        self.selected = False
+
+    def __str__(self):
+        return str(boardSpot.value)
+
+    def isMine(self):
+        if boardSpot.value == -1:
+            return True
+        return False
+
+
+class boardClass(object):
+    def __init__(self, m_boardSize, m_numMines):
+        self.board = [[boardSpot() for i in range(m_boardSize)] for j in range(m_boardSize)]
+        self.boardSize = m_boardSize
+        self.numMines = m_numMines
+        self.selectableSpots = m_boardSize * m_boardSize - m_numMines
+        i = 0
+        while i < m_numMines:
+            x = random.randint(0, self.boardSize-1)
+            y = random.randint(0, self.boardSize-1)
+            if not self.board[x][y].mine:
+                self.addMine(x, y)
+                i += 1
+            else:
+                i -= 1
+
+    def __str__(self):
+        returnString = " "
+        divider = "\n---"
+
+        for i in range(0, self.boardSize):
+            returnString += " | " + str(i)
+            divider += "----"
+        divider += "\n"
+
+        returnString += divider
+        for y in range(0, self.boardSize):
+            returnString += str(y)
+            for x in range(0, self.boardSize):
+                if self.board[x][y].mine and self.board[x][y].selected:
+                    returnString += " |" + str(self.board[x][y].value)
+                elif self.board[x][y].selected:
+                    returnString += " | " + str(self.board[x][y].value)
+                else:
+                    returnString += " |  "
+            returnString += " |"
+            returnString += divider
+        return returnString
+
+    def addMine(self, x, y):
+        self.board[x][y].value = -1
+        self.board[x][y].mine = True
+        j = 2 # Denomination Size
+
+        for colIndex in range(x-j,x+j+1):
+            for rowIndex in range(y-j,y+j+1):
+                if ((0 <= colIndex) and (colIndex < self.boardSize)):
+                    if ((0 <= rowIndex) and (rowIndex < self.boardSize)):
+                        if (not self.board[colIndex][rowIndex].mine):
+                            self.board[colIndex][rowIndex].value +=1
+
+
+    def makeMove(self, x, y):
+        if (self.board[x][y].selected == True):
+            print(" ")
+            print("Already selected. Please select a new spot")
+            print(" ")
+        else:
+            self.board[x][y].selected = True
+            self.selectableSpots -= 1
+            if self.board[x][y].value == -1:
+                return False
+            if self.board[x][y].value == 0:
+                for i in range(x-1, x+2):
+                    if i >= 0 and i < self.boardSize:
+                        if y-1 >= 0 and not self.board[i][y-1].selected:
+                            self.makeMove(i, y-1)
+                        if y+1 < self.boardSize and not self.board[i][y+1].selected:
+                            self.makeMove(i, y+1)
+                if x-1 >= 0 and not self.board[x-1][y].selected:
+                    self.makeMove(x-1, y)
+                if x+1 < self.boardSize and not self.board[x+1][y].selected:
+                    self.makeMove(x+1, y)
+                return True
+            else:
+                return True
+
+
+    def makeSafeMove(self,x,y):
+        if (self.board[x][y].selected == False):
+            if self.board[x][y].value == -1:
+                return False
+            self.board[x][y].selected = True
+            self.selectableSpots -= 1
+            if self.board[x][y].value == 0:
+                for i in range(x-1, x+2):
+                    if i >= 0 and i < self.boardSize:
+                        if y-1 >= 0 and not self.board[i][y-1].selected:
+                            self.makeMove(i, y-1)
+                        if y+1 < self.boardSize and not self.board[i][y+1].selected:
+                            self.makeMove(i, y+1)
+                if x-1 >= 0 and not self.board[x-1][y].selected:
+                    self.makeMove(x-1, y)
+                if x+1 < self.boardSize and not self.board[x+1][y].selected:
+                    self.makeMove(x+1, y)
+                return True
+            else:
+                return True
+
+    def hitMine(self, x, y):
+        return self.board[x][y].value == -1
+
+    def isWinner(self):
+        return self.selectableSpots == 0
+
+from random import randint
+
+
+#play game
+def playGame():
+    boardSize = int(input("Choose the Width of the board: "))
+    numMines = int(input("Choose the number of mines: "))
+    gameOver = False
+    winner = False
+    Board = boardClass(boardSize, numMines)
+
+    for i in range(boardSize*boardSize/2):
+        x = randint(0, boardSize-1)
+        y = randint(0, boardSize-1)
+        Board.makeSafeMove(x,y)
+
+
+    while not gameOver:
+        print(Board)
+        print("Make your move:")
+        x = int(input("x: "))
+        y = int(input("y: "))
+        Board.makeMove(x, y)
+        gameOver = Board.hitMine(x, y)
+        if Board.isWinner() and gameOver == False:
+            gameOver = True
+            winner = True
+
+    print(Board)
+    if winner:
+        print("Congratulations, You Win!")
+    else:
+        print("You hit a mine, Game Over!")
+
+playGame()
